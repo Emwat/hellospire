1887df03393fd5b4994fac1a3726f3a3ad93c5ff
emwat <tawme.noo@gmail.com>
2025-04-30 08:41:09 -0400

    initial commit

---
+++ src/main/java/hellospire/cards/DashStrike1.java
@@ -0,0 +1,48 @@
+package hellospire.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.unique.PoisonLoseHpAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hellospire.character.MyCharacter;
+import hellospire.util.CardStats;
+
+public class DashStrike1 extends BaseCard {
+    public static final String ID = makeID("Dash Strike 1");
+    private static final CardStats info = new CardStats(
+            MyCharacter.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int DAMAGE = 5;
+    private static final int UPG_DAMAGE = 2;
+    private static final int POISON_DAMAGE = 1;
+    private static final int UPG_POISON_DAMAGE = 2;
+
+    public DashStrike1() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+        setMagic(POISON_DAMAGE, UPG_POISON_DAMAGE);
+
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        addToBot(new PoisonLoseHpAction(m, m, magicNumber, AbstractGameAction.AttackEffect.POISON));
+    }
+
+    @Override
+    public AbstractCard makeCopy() { //Optional
+        return new DashStrike1();
+    }
+}
